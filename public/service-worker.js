c5eb26f37d96c51ef46082c7549dce76378f50c7
Edelgard <jtboyman@gmail.com>
2022-02-12 20:28:48 -0500

    half working service worker, error on installation

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./js/index.js",
+    "./js/idb.js",
+    "./css/styles.css",
+    "./index.hmtl"
+]
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
